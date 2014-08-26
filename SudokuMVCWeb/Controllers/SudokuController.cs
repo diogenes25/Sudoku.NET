5ef4b5ff474361c628bc2ad1b87dac3e839abca7
onn <t.onnen@bo-wohnungswirtschaft.de>
2014-08-26 22:30:04 +0200

    Added Solvetechniques to web

--- SudokuMVCWeb/Controllers/SudokuController.cs
+++ SudokuMVCWeb/Controllers/SudokuController.cs
@@ -1,11 +1,22 @@
 ﻿using DE.Onnen.Sudoku;
+using DE.Onnen.Sudoku.SolveTechniques;
 using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace SudokuMVCWeb.Controllers
 {
 	public class SudokuController : Controller
 	{
+        private IList<ASolveTechnique> st;
+
+        public SudokuController()
+        {
+            st = new List<ASolveTechnique>();
+            st.Add(new DE.Onnen.Sudoku.SolveTechniques.NakedPairTrippleQuad());
+            st.Add(new DE.Onnen.Sudoku.SolveTechniques.HiddenPairTripleQuad());
+            st.Add(new DE.Onnen.Sudoku.SolveTechniques.LockedCandidates());
+        }
 		// GET: Sudoku
 		//public ActionResult Index()
 		//{
@@ -14,11 +25,12 @@ namespace SudokuMVCWeb.Controllers
 
 		public ActionResult Index(string s)
 		{
-			Board board = new Board();
+			Board board = new Board(st);
 			if (!String.IsNullOrWhiteSpace(s))
 			{
 				board.SetCellsFromString(s);
 			}
+            board.Solve(new SudokuLog());
 			return View(board);
 		}
 	}
